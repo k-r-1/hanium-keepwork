67ae1b4e8f4844b5902a854cf4da4d91edbf5c8e
catapillar0505 <twrp3301@gmail.com>
2023-07-18 22:31:00 +0900

    지역 선택 화면 구현

--- app/src/main/java/com/example/a23_hf069/RegionFragment.kt
+++ app/src/main/java/com/example/a23_hf069/RegionFragment.kt
@@ -26,7 +26,9 @@ import java.net.HttpURLConnection
 import java.net.URL
 
 class RegionFragment : Fragment() {
-    private lateinit var regionList: List<Region>
+    private var s_regionList: List<Region> = emptyList()
+    private var m_regionList: List<Region> = emptyList()
+    private var regionList: List<Region> = emptyList()
     private lateinit var listView1: ListView
     private lateinit var listView2: ListView
 
@@ -43,21 +45,21 @@ class RegionFragment : Fragment() {
 
 
         // API 호출
-        fetchJobData()
+        fetchRegionData()
 
         return view
     }
 
-    private fun fetchJobData() {
+    private fun fetchRegionData() {
         val url =
             "http://openapi.work.go.kr/opi/commonCode/commonCode.do?returnType=XML&target=CMCD&authKey=WNLJYZLM2VZXTT2TZA9XR2VR1HK&dtlGb=1"
-        FetchJobData().execute(url)
+        FetchRegionData().execute(url)
     }
 
-    private inner class FetchJobData : AsyncTask<String, Void, List<Job>>() {
-        override fun doInBackground(vararg urls: String): List<Job> {
+    private inner class FetchRegionData : AsyncTask<String, Void, List<Region>>() {
+        override fun doInBackground(vararg urls: String): List<Region> {
             val urlString = urls[0]
-            var result: List<Job> = emptyList()
+            var result: List<Region> = emptyList()
             var connection: HttpURLConnection? = null
 
             try {
@@ -81,11 +83,8 @@ class RegionFragment : Fragment() {
             return result
         }
 
-        private fun parseXml(inputStream: InputStream): List<Job> {
-            // 지역 대분류 리스트 (시/도)
-            val superRegionList = mutableListOf<String>()
-            // 지역 중분류 리스트 (시/군/구)
-            val middleRegionList = mutableListOf<String>()
+        private fun parseXml(inputStream: InputStream): List<Region> {
+            regionList = listOf<Region>()
             val factory = XmlPullParserFactory.newInstance()
             val xpp = factory.newPullParser()
             xpp.setInput(inputStream, null)
@@ -93,22 +92,17 @@ class RegionFragment : Fragment() {
             var eventType = xpp.eventType
             var regionCd: String? = null // 지역 분류 코드
             var regionNm: String? = null // 지역 이름
-            var superCd: String? = null // 지역 대분류 코드
 
 
             while (eventType != XmlPullParser.END_DOCUMENT) {
                 when (eventType) {
                     XmlPullParser.START_TAG -> {
-                        when (xpp.name) {
+                        if (xpp.name=="oneDepth") { // 대분류
                             "regionCd" -> regionCd = xpp.nextText()
                             "regionNm" -> regionNm = xpp.nextText()
-                        }
-                    }
-                    XmlPullParser.END_TAG -> {
-                        if (xpp.name == "cmcdRegion") {
                             regionCd?.let { c ->
                                 regionNm?.let { n ->
-                                    regionList.add(
+                                    s_regionList.add(
                                         Region(
                                             c, n
                                         )
@@ -117,172 +111,93 @@ class RegionFragment : Fragment() {
                             }
                             regionCd = null
                             regionNm = null
+                            regionList = s_regionList
+
+                        } else if (xpp.name == "twoDepth") { // 중분류
+                            "regionCd" -> regionCd = xpp.nextText()
+                            "regionNm" -> regionNm = xpp.nextText()
+                            regionCd?.let { c ->
+                                regionNm?.let { n ->
+                                    m_regionList.add(
+                                        Region(
+                                            c, n
+                                        )
+                                    )
+                                }
+                            }
+                            regionList = m_regionList
+                            regionCd = null
+                            regionNm = null
 
                         }
                     }
+
                 }
                 eventType = xpp.next()
             }
-
             return regionList
         }
 
         override fun onPostExecute(result: List<Region>) {
             regionList = result
-            showJobList()
+            showRegionList()
         }
     }
 
-    private fun showJobList() {
-        val adapter = CustomAdapter(this@WantedWorkNetFragment, jobList)
-        jobListView.adapter = adapter
+    private fun showRegionList() {
+        val adapter1 = UIAdapter(this@RegionFragment, s_regionList)
+        val adapter2 = UIAdapter(this@RegionFragment, m_regionList)
+        listView1.adapter = adapter1
+        listView2.adapter = adapter2
 
-        jobListView.setOnItemClickListener { _, _, position, _ ->
-            val job = jobList[position]
-            val intent = JobDetailActivity.newIntent(requireContext(), job)
-            startActivity(intent)
+        listView1.setOnItemClickListener { _, _, position, _ ->
+
+        }
+        listView2.setOnItemClickListener { _, _, position, _ ->
+
         }
     }
 }
 
-class CustomAdapter(private val fragment: Fragment, private val jobList: List<Job>) :
-    ArrayAdapter<Job>(fragment.requireActivity(), R.layout.job_item, jobList) {
+class UIAdapter(private val fragment: Fragment, private val RegionList: List<Region>) :
+    ArrayAdapter<Region>(fragment.requireActivity(), R.layout.filtering_list_item , RegionList) {
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-        val view =
-            convertView ?: LayoutInflater.from(context).inflate(R.layout.job_item, parent, false)
+        val view = convertView ?: LayoutInflater.from(context).inflate(R.layout.filtering_list_item, parent, false)
 
-        val job = jobList[position]
+        val region = RegionList[position]
 
-        val titleTextView: TextView = view.findViewById(R.id.titleTextView)
-        val companyTextView: TextView = view.findViewById(R.id.companyTextView)
-        val regionContTextView: TextView = view.findViewById(R.id.regionContTextView)
-
-        titleTextView.text = job.title
-        companyTextView.text = job.company
-        regionContTextView.text = job.region
+        val region_btn: Button = view.findViewById(R.id.region_btn)
 
         return view
     }
 }
 
-class JobDetailActivity : AppCompatActivity() {
 
-    private lateinit var company: TextView // 회사명
-    private lateinit var title: TextView // 제목
-    private lateinit var salTpNm: TextView // 임금형태
-    private lateinit var sal: TextView // 급여
-    private lateinit var region: TextView // 근무지역
-    private lateinit var holidayTpNm: TextView // 근무형태
-    private lateinit var minEdubg: TextView // 최소학력
-    private lateinit var career: TextView // 경력
-    private lateinit var closeDt: TextView // 마감일자
-    private lateinit var wantedMobileInfoUrl: TextView // 워크넷 모바일 채용정보 URL
-    private lateinit var jobsCd: TextView // 직종코드
-
-    companion object {
-        private const val JOB_EXTRA = "job"
-
-        fun newIntent(context: Context, job: Job): Intent {
-            return Intent(context, JobDetailActivity::class.java).apply {
-                putExtra(JOB_EXTRA, job)
-            }
-        }
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_job_detail)
-
-        // 기본 툴바 숨기기
-        val actionBar: ActionBar? = supportActionBar
-        if (actionBar != null) {
-            actionBar.hide()
-        }
-
-        company = findViewById(R.id.company)
-        title = findViewById(R.id.title)
-        salTpNm = findViewById(R.id.salTpNm)
-        sal = findViewById(R.id.sal)
-        region = findViewById(R.id.region)
-        holidayTpNm = findViewById(R.id.holidayTpNm)
-        minEdubg = findViewById(R.id.minEdubg)
-        career = findViewById(R.id.career)
-        closeDt = findViewById(R.id.closeDt)
-        wantedMobileInfoUrl = findViewById(R.id.wantedMobileInfoUrl)
-        jobsCd = findViewById(R.id.jobsCd)
-
-        val job = intent.getParcelableExtra<Job>(JOB_EXTRA)
-
-        company.text = job?.company
-        title.text = job?.title
-        salTpNm.text = job?.salTpNm
-        sal.text = job?.sal
-        region.text = job?.region
-        holidayTpNm.text = job?.holidayTpNm
-        minEdubg.text = job?.minEdubg
-        career.text = job?.career
-        closeDt.text = job?.closeDt
-        wantedMobileInfoUrl.text = job?.wantedMobileInfoUrl
-        jobsCd.text = job?.jobsCd
-
-        backButton.setOnClickListener {
-            onBackPressed()
-        }
-    }
-}
-
-data class Job(
-    val company: String,
-    val title: String,
-    val salTpNm: String?,
-    val sal: String?,
-    val region: String?,
-    val holidayTpNm: String?,
-    val minEdubg: String?,
-    val career: String?,
-    val closeDt: String?,
-    val wantedMobileInfoUrl: String?,
-    val jobsCd: String?
+data class Region(
+    val regionCd: String,
+    val regionNm: String,
 ) : Parcelable {
     constructor(parcel: Parcel) : this(
-        parcel.readString() ?: "",
-        parcel.readString() ?: "",
-        parcel.readString() ?: "",
-        parcel.readString() ?: "",
-        parcel.readString() ?: "",
-        parcel.readString() ?: "",
-        parcel.readString() ?: "",
-        parcel.readString() ?: "",
-        parcel.readString() ?: "",
         parcel.readString() ?: "",
         parcel.readString() ?: ""
     )
 
     override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeString(company)
-        parcel.writeString(title)
-        parcel.writeString(salTpNm)
-        parcel.writeString(sal)
-        parcel.writeString(region)
-        parcel.writeString(holidayTpNm)
-        parcel.writeString(minEdubg)
-        parcel.writeString(career)
-        parcel.writeString(closeDt)
-        parcel.writeString(wantedMobileInfoUrl)
-        parcel.writeString(jobsCd)
+        parcel.writeString(regionCd)
+        parcel.writeString(regionNm)
     }
 
     override fun describeContents(): Int {
         return 0
     }
 
-    companion object CREATOR : Parcelable.Creator<Job> {
-        override fun createFromParcel(parcel: Parcel): Job {
-            return Job(parcel)
+    companion object CREATOR : Parcelable.Creator<Region> {
+        override fun createFromParcel(parcel: Parcel): Region {
+            return Region(parcel)
         }
 
-        override fun newArray(size: Int): Array<Job?> {
+        override fun newArray(size: Int): Array<Region?> {
             return arrayOfNulls(size)
         }
     }

4f2b0e728bb3a23e25bb0d6828b6783fd6aacba5
seoyoung <si159521@gmail.com>
2023-07-19 20:39:53 +0900

    경력, 학력, 마감일 필터링2

--- app/src/main/java/com/example/a23_hf069/WantedFilteringFragment.kt
+++ app/src/main/java/com/example/a23_hf069/WantedFilteringFragment.kt
@@ -39,23 +39,26 @@ class WantedFilteringFragment : Fragment()  {
     private var selectedCareer: Int = 0  // 0: 전체, 1: 신입, 2: 경력, ...
     private var selectedCloseDate: Int = 0  // 0: 전체, 1: 1일 이내, 2: 3일 이내, ...
 
+    private val selectedEducationList: MutableSet<Int> = mutableSetOf()
+    private val selectedCareerList: MutableSet<Int> = mutableSetOf()
+    private val selectedCloseDateList: MutableSet<Int> = mutableSetOf()
+
+    lateinit var complete_btn1 : Button //완료버튼
+
     // 화면 띄우기
     override fun onCreateView( // onCreateView 함수 오버라이드 해줌
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
 
-    ): View?
-    {
+    ): View? {
         return inflater.inflate(R.layout.fragment_wanted_filtering, container, false)
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         //지역
         region_btn = view.findViewById<Button>(R.id.region_btn)
-
 
         //학력 체크
         edu_btn1 = view.findViewById<Button>(R.id.cb_e_1) //전체
@@ -88,49 +91,58 @@ class WantedFilteringFragment : Fragment()  {
         }
 */
         // 학력 버튼 클릭 리스너
-        edu_btn1.setOnClickListener { setSelectedEducation(0) }  // 전체
-        edu_btn2.setOnClickListener { setSelectedEducation(1) }  // 초등학교
-        edu_btn3.setOnClickListener { setSelectedEducation(2) }  // 중학교
-        edu_btn4.setOnClickListener { setSelectedEducation(3) }  // 고등학교
-        edu_btn5.setOnClickListener { setSelectedEducation(4) }  // 대학(2년제)
-        edu_btn6.setOnClickListener { setSelectedEducation(5) }  // 대학(4년제)
+        edu_btn1.setOnClickListener { SelectedEducation(0) }  // 전체
+        edu_btn2.setOnClickListener { SelectedEducation(1) }  // 초등학교
+        edu_btn3.setOnClickListener { SelectedEducation(2) }  // 중학교
+        edu_btn4.setOnClickListener { SelectedEducation(3) }  // 고등학교
+        edu_btn5.setOnClickListener { SelectedEducation(4) }  // 대학(2년제)
+        edu_btn6.setOnClickListener { SelectedEducation(5) }  // 대학(4년제)
 
         // 경력 버튼 클릭 리스너
-        career_btn1.setOnClickListener { setSelectedCareer(0) }  // 전체
-        career_btn2.setOnClickListener { setSelectedCareer(1) }  // 신입
-        career_btn3.setOnClickListener { setSelectedCareer(2) }  // 경력
+        career_btn1.setOnClickListener { SelectedCareer(0) }  // 전체
+        career_btn2.setOnClickListener { SelectedCareer(1) }  // 신입
+        career_btn3.setOnClickListener { SelectedCareer(2) }  // 경력
 
         // 마감일 버튼 클릭 리스너
-        closeDt_btn1.setOnClickListener { setSelectedCloseDate(0) }  // 전체
-        closeDt_btn2.setOnClickListener { setSelectedCloseDate(1) }  // 1일 이내
-        closeDt_btn3.setOnClickListener { setSelectedCloseDate(2) }  // 3일 이내
-        closeDt_btn4.setOnClickListener { setSelectedCloseDate(3) }  // 7일 이내
-        closeDt_btn5.setOnClickListener { setSelectedCloseDate(4) }  // 14일 이내
-        closeDt_btn6.setOnClickListener { setSelectedCloseDate(5) }  // 30일 이내
+        closeDt_btn1.setOnClickListener { SelectedCloseDate(0) }  // 전체
+        closeDt_btn2.setOnClickListener { SelectedCloseDate(1) }  // 1일 이내
+        closeDt_btn3.setOnClickListener { SelectedCloseDate(2) }  // 3일 이내
+        closeDt_btn4.setOnClickListener { SelectedCloseDate(3) }  // 7일 이내
+        closeDt_btn5.setOnClickListener { SelectedCloseDate(4) }  // 14일 이내
+        closeDt_btn6.setOnClickListener { SelectedCloseDate(5) }  // 30일 이내
 
+    //완료버튼 누르면 필터링된 공고를 WantedFilteredFragment로 전환
+        complete_btn1 = view.findViewById<Button>(R.id.complete_btn1)
+        complete_btn1.setOnClickListener {
+            val wantedFilteredFragment = WantedFilteredFragment()
+            requireActivity().supportFragmentManager.beginTransaction()
+                .replace(R.id.fl_container, wantedFilteredFragment)
+                .addToBackStack(null)
+                .commit()
+        } //만약 조건선택이 완료되지 않은 채 완료버튼을 눌렀다면? ->
     }
 
-    private fun setSelectedEducation(selected: Int) {
+    private fun SelectedEducation(selected: Int) {
         if (selectedEducation == 0 || selected > selectedEducation) {
             selectedEducation = selected
-            // 1번이나 2번이 선택되었을 때, 그리고 0번과 같은 결과를 출력
+            // 1번이나 2번이 선택되었을 때(혹은 둘다), 0번과 같은 결과를 출력(초졸, 중졸은 학력무관과 마찬가지)
             if ((selectedEducation == 1 || selectedEducation == 2) && selectedEducation != 0) {
-                // 0번과 같은 결과를 출력하는 로직을 작성합니다.
-                println("Selected Education: 0 (Same as 0)")
-                // 추가적인 처리를 수행할 수 있습니다.
+                // 0번과 같은 결과
+
+
             }
         }
         filterItems()
     }
 
-    private fun setSelectedCareer(selected: Int) {
+    private fun SelectedCareer(selected: Int) {
         if (selectedCareer == 0 || selected > selectedCareer) {
             selectedCareer = selected
         }
         filterItems()
     }
 
-    private fun setSelectedCloseDate(selected: Int) {
+    private fun SelectedCloseDate(selected: Int) {
         if (selectedCloseDate == 0 || selected > selectedCloseDate) {
             selectedCloseDate = selected
         }
@@ -138,11 +150,20 @@ class WantedFilteringFragment : Fragment()  {
     }
 
     private fun filterItems() {
-        // 선택한 조건에 따라 필터링된 항목을 출력하는 로직을 작성합니다.
-        // 예시로서 선택된 조건을 출력하는 코드를 작성합니다.
-        println("Selected Education: $selectedEducation")
-        println("Selected Career: $selectedCareer")
-        println("Selected Close Date: $selectedCloseDate")
+        // 선택한 조건에 따라 필터링된 항목을 리스트에 저장(중복값을 찾기위해)
+        val filteredEducationList = if (selectedEducationList.isEmpty()) listOf(0) else selectedEducationList
+        val filteredCareerList = if (selectedCareerList.isEmpty()) listOf(0) else selectedCareerList
+        val filteredCloseDateList = if (selectedCloseDateList.isEmpty()) listOf(0) else selectedCloseDateList
+
+        // 세 리스트에서 중복되는 값들을 찾습니다.
+        val intersectedItems = filteredEducationList.intersect(filteredCareerList).intersect(filteredCloseDateList)
+
+        for (item in intersectedItems) {
+            println(item)
+            // 필요한 처리를 수행합니다.
+        }
     }
 
-}
+
+
+    }

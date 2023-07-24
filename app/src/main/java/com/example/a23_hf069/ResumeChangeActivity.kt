06c5168d3fc224c2fe3690fcb8bceea33cbcd7f5
PSG <green7170@swu.ac.kr>
2023-07-24 22:50:00 +0900

    이력서 불러오기

--- app/src/main/java/com/example/a23_hf069/ResumeChangeActivity.kt
+++ app/src/main/java/com/example/a23_hf069/ResumeChangeActivity.kt
@@ -7,6 +7,7 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import com.google.gson.Gson
 import okhttp3.*
 import java.io.IOException
 
@@ -26,6 +27,7 @@ class ResumeChangeActivity : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        fetchResumeDataFromServer()
         setContentView(R.layout.activity_resume_change)
 
 // Get user ID
@@ -94,6 +96,68 @@ class ResumeChangeActivity : AppCompatActivity() {
             )
 
             Toast.makeText(this, "이력서가 작성완료되었습니다", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    // 이력서 정보를 불러오기 위한 메서드
+    private fun fetchResumeDataFromServer() {
+        val url = "http://$IP_ADDRESS/android_resume_change.php" // 이력서 정보를 가져올 서버 주소
+
+        val client = OkHttpClient()
+
+        val formBody = FormBody.Builder()
+            .add("personal_id", userId) // 사용자 ID를 서버에 전달
+            .build()
+
+        val request = Request.Builder()
+            .url(url)
+            .post(formBody)
+            .build()
+
+        client.newCall(request).enqueue(object : okhttp3.Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                // 요청 실패 처리
+                e.printStackTrace()
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                // 요청 성공 시 호출되는 콜백 메서드
+                val responseData = response.body?.string()
+                // responseData를 파싱하여 이력서 정보를 화면에 표시
+                if (responseData != null) {
+                    runOnUiThread {
+                        displayResumeData(responseData)
+                    }
+                }
+            }
+        })
+    }
+
+    // 서버로부터 받은 이력서 정보를 화면에 표시
+    private fun displayResumeData(responseData: String) {
+        val gson = Gson()
+        val resumeData = gson.fromJson(responseData, ResumeData::class.java)
+
+        // resumeData가 null인 경우 처리
+        if (resumeData == null) {
+            Toast.makeText(this, "이력서 정보를 가져오지 못했습니다.", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        // 서버로부터 받은 이력서 정보를 EditText 등의 뷰에 표시
+        editResumeTitle.setText(resumeData.resume_title)
+        editTextAcademic.setText(resumeData.resume_academic)
+        editTextCareer.setText(resumeData.resume_career)
+        editTextIntroduction.setText(resumeData.resume_introduction)
+        editTextCertificate.setText(resumeData.resume_certificate)
+        editTextEducation.setText(resumeData.resume_learning)
+        editTextDesire.setText(resumeData.resume_desire)
+
+        // 이력서 작성 상태에 따라 버튼 텍스트 변경
+        if (resumeData.resume_complete == "작성 중") {
+            buttonSubmit1.text = "임시저장"
+        } else {
+            buttonSubmit2.text = "작성완료"
         }
     }
 

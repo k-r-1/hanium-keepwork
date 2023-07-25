ce58aca97a61da69906831244121f36540960801
PSG <green7170@swu.ac.kr>
2023-07-25 23:43:59 +0900

    이력서 수정화면 인텐트 연결

--- app/src/main/java/com/example/a23_hf069/ResumeFragment.kt
+++ app/src/main/java/com/example/a23_hf069/ResumeFragment.kt
@@ -25,7 +25,7 @@ import java.io.IOException
 
 class ResumeFragment : Fragment() {
     // 서버의 IP 주소를 저장할 변수
-    private var IP_ADDRESS = "54.180.120.162"
+    private var IP_ADDRESS = "15.165.160.71"
 
     // 사용자 ID를 저장할 변수
     private lateinit var userId: String

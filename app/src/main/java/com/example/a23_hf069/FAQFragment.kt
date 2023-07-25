b3344cea9784f228b359e1a0968c2936232147bc
박주원 <juwon_0715@naver.com>
2023-07-26 01:49:29 +0900

    자주 묻는 질문 화면(FAQFragment) - TabLayout(개인회원/기업회원) 완성

--- app/src/main/java/com/example/a23_hf069/FAQFragment.kt
+++ app/src/main/java/com/example/a23_hf069/FAQFragment.kt
@@ -5,18 +5,52 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageButton
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
 
-class FAQFragment : Fragment() {
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        val rootView = inflater.inflate(R.layout.fragment_f_a_q, container, false)
+class FAQFragment : Fragment(), TabLayout.OnTabSelectedListener {
 
-        val closeButton = rootView.findViewById<ImageButton>(R.id.backButton)
+    private lateinit var viewPager: ViewPager
+    private lateinit var tabLayout: TabLayout
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_f_a_q, container, false)
+
+        val adapter = PagerAdapter(childFragmentManager)
+        adapter.addFragment(FAQPersonalFragment(), "개인회원")
+        adapter.addFragment(FAQCorporateFragment(), "기업회원")
+
+        viewPager = view.findViewById<ViewPager>(R.id.viewpager01)
+        viewPager.adapter = adapter
+
+        tabLayout = view.findViewById<TabLayout>(R.id.tablayout01)
+        tabLayout.setupWithViewPager(viewPager)
+        tabLayout.addOnTabSelectedListener(this)
+
+        val closeButton = view.findViewById<ImageButton>(R.id.backButton)
         closeButton.setOnClickListener {
             requireActivity().onBackPressed()
         }
 
-        return rootView
+        return view
     }
+
+    override fun onTabSelected(tab: TabLayout.Tab?) {
+        // Do nothing
+    }
+
+    override fun onTabUnselected(tab: TabLayout.Tab?) {
+        // Do nothing
+    }
+
+    override fun onTabReselected(tab: TabLayout.Tab?) {
+        // Do nothing
+    }
+
 }
---
+++ app/src/main/java/com/example/a23_hf069/FAQPersonalFragment.kt
@@ -0,0 +1,18 @@
+package com.example.a23_hf069
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class FAQPersonalFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_f_a_q_personal, container, false)
+    }
+}

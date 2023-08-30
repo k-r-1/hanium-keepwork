687ab91ccdc6445a79e5ff89eb1b397fb54c2a1b
박주원 <juwon_0715@naver.com>
2023-08-30 17:19:28 +0900

    기업회원 알림 화면(CorporateHomeNotificationFragment) 구현

--- app/src/main/java/com/example/a23_hf069/CorporateHomeFragment.kt
+++ app/src/main/java/com/example/a23_hf069/CorporateHomeFragment.kt
@@ -5,27 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.ImageView
+import androidx.appcompat.widget.Toolbar
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [CorporateHomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class CorporateHomeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    private lateinit var notificationButton: ImageView
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // 툴바에서 notificationButton을 찾아 클릭 리스너를 설정
+        notificationButton = view.findViewById(R.id.notifications)
+        notificationButton.setOnClickListener {
+            // notificationButton을 클릭하면 알림 화면 CorporateHomeNotificationFragment로 전환
+            val notificationFragment = CorporateHomeNotificationFragment()
+            requireActivity().supportFragmentManager.beginTransaction()
+                .replace(R.id.fl_container, notificationFragment)
+                .addToBackStack(null)
+                .commit()
         }
     }
 
@@ -37,23 +36,5 @@ class CorporateHomeFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_corporate_home, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment CorporateHomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            CorporateHomeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+
 }

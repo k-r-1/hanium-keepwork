04029e6fa85e9d7c858ccc3d8a64c60c8adfa502
greenwithtea <keilly0104@naver.com>
2023-08-30 21:15:22 +0900

    공고관리화면 임시로 이미지 얹음

--- app/src/main/java/com/example/a23_hf069/JobManangementPostFragment.kt
+++ app/src/main/java/com/example/a23_hf069/JobManangementPostFragment.kt
@@ -1,59 +1,117 @@
 package com.example.a23_hf069
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import okhttp3.*
+import org.json.JSONArray
+import java.io.IOException
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class JobManagementPostFragment : Fragment() {
 
-/**
- * A simple [Fragment] subclass.
- * Use the [JobManangementPostFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class JobManangementPostFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var IP_ADDRESS = "54.180.186.168"
+    private val recyclerViewAdapter = JobPostAdapter()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_job_manangement_post, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment JobManangementPostFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            JobManangementPostFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val recyclerView = view.findViewById<RecyclerView>(R.id.recylcerviewJobPost)
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        recyclerView.adapter = recyclerViewAdapter
+
+        fetchJobPosts()
+    }
+
+    private fun fetchJobPosts() {
+        val client = OkHttpClient()
+        val phpUrl = "http://$IP_ADDRESS/android_recruit_post.php"
+        val requestBody = FormBody.Builder()
+            .add("company_email", "keepwork@naver.com")
+            .build()
+        val request = Request.Builder()
+            .url(phpUrl)
+            .post(requestBody)
+            .build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onResponse(call: Call, response: Response) {
+                val responseData = response.body?.string()
+                val jobPosts = parseJobPosts(responseData)
+
+                GlobalScope.launch(Dispatchers.Main) {
+                    recyclerViewAdapter.setJobPosts(jobPosts)
                 }
             }
+
+            override fun onFailure(call: Call, e: IOException) {
+                e.printStackTrace()
+            }
+        })
     }
+
+    private fun parseJobPosts(json: String?): List<JobPost> {
+        val jobPosts = mutableListOf<JobPost>()
+        try {
+            val jsonArray = JSONArray(json)
+            for (i in 0 until jsonArray.length()) {
+                val jsonObject = jsonArray.getJSONObject(i)
+                val title = jsonObject.getString("recruitTitle")
+                val startDate = jsonObject.getString("recruitStart")
+                val endDate = jsonObject.getString("recruitEnd")
+                jobPosts.add(JobPost(title, startDate, endDate))
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        return jobPosts
+    }
+}
+
+data class JobPost(val title: String, val startDate: String, val endDate: String)
+
+class JobPostAdapter : RecyclerView.Adapter<JobPostAdapter.ViewHolder>() {
+    private val jobPosts = mutableListOf<JobPost>()
+
+    fun setJobPosts(posts: List<JobPost>) {
+        jobPosts.clear()
+        jobPosts.addAll(posts)
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.jobmanagement_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val jobPost = jobPosts[position]
+        holder.itemView.apply {
+            findViewById<TextView>(R.id.tvJobManagementTitle).text = jobPost.title
+            findViewById<TextView>(R.id.tvJobManagentStartDate).text = jobPost.startDate
+            findViewById<TextView>(R.id.tvJobManagementEndDate).text = jobPost.endDate
+            // Set click listeners and other UI updates as needed
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return jobPosts.size
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
 }

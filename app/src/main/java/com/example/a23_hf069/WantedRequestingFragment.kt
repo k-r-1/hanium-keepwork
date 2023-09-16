3407cc496ec3568aa45c5f49dc4ef7e970b0b851
seoyoung <si159521@gmail.com>
2023-09-16 22:35:51 +0900

    즉시지원

--- app/src/main/java/com/example/a23_hf069/WantedRequestingFragment.kt
+++ app/src/main/java/com/example/a23_hf069/WantedRequestingFragment.kt
@@ -7,8 +7,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class WantedRequestingFragment : Fragment() {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: ImmediateJobAdapter
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -16,12 +22,22 @@ class WantedRequestingFragment : Fragment() {
         val rootView= inflater.inflate(R.layout.fragment_wanted_requesting, container, false)
 
         //아래 코드는 중간평가 주작을 위해 작성한 코드임
-        val mainLayout = rootView.findViewById<LinearLayout>(R.id.myLinearLayout)
-        mainLayout.setOnClickListener {
-            // '채용공고 상세정보' 페이지로 이동
-            val intent = Intent(activity, JobDetailActivity::class.java)
-            startActivity(intent)
-        }
+//        val mainLayout = rootView.findViewById<LinearLayout>(R.id.myLinearLayout)
+//        mainLayout.setOnClickListener {
+//            // '채용공고 상세정보' 페이지로 이동
+//            val intent = Intent(activity, JobDetailActivity::class.java)
+//            startActivity(intent)
+//        }
+
+        val jobs = listOf(
+            ImmediateJob("한결소래빌리지 사회복지사 모집 공고", "한결 소래빌리지", "인천광역시 남동구"),
+            ImmediateJob("안성시지체장애인협회 직원 모집", "한국지체장애인협회안성시지회", "경기도 안성시")
+        )
+
+        recyclerView = rootView.findViewById(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        adapter = ImmediateJobAdapter(jobs)
+        recyclerView.adapter = adapter
 
         return rootView
     }
